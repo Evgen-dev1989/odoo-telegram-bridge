acc7cf261c8d7bc54c1b8cea487b9eea44ce9915
Evgen-dev1989 <81103637+Evgen-dev1989@users.noreply.github.com>
2026-05-25 12:13:44 +0300

    +

--- custom_addons/telegram_odoo_bot/__manifest__.py
+++ custom_addons/telegram_odoo_bot/__manifest__.py
@@ -3,9 +3,9 @@
     'version': '1.0',
     'category': 'Sales/CRM',
     'depends': ['crm'],
-    'data': [],  # ТУТ СТРОГО ПУСТО
+    'data': [],  
     'installable': True,
     'license': 'LGPL-3',
 }
 
-#python odoo/odoo-bin -c odoo.conf -d odoo_test_db -i my_crm_extension
+#python odoo/odoo-bin -c odoo.conf -d odoo_test_db -i my_crm_extensionstart
